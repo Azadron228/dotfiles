c8aaeee3428f3f0d9aa30fb8e51a5beb66f405b9
Azadron228 <azamat.ibrayv@gmail.com>
2024-05-09 14:13:16 +0500

    nvim updated

---
+++ nvim/lua/plugins/blankline.lua
@@ -0,0 +1,9 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+}
